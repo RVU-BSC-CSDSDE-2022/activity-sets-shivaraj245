4c30ab72b5de81af259e6cb4c22e24341ff2e5f0
Shivaraj 	 Manikashetti <121168953+shivaraj245@users.noreply.github.com>
2022-12-21 12:25:36 +0000

    something

---
+++ set01/problem01.c
@@ -0,0 +1,6 @@
+// 1. Write a C program to print your name.
+#include<stdio.h>
+int main(){
+  printf("my name is shivu\n");
+  return 0;
+}
---
+++ set01/problem02.c
@@ -0,0 +1,10 @@
+// 2. Write a C program to add two numbers.
+#include<stdio.h>
+int main(){
+  int a=25;
+  int b=35;
+  int c;
+  c=a+b;
+  printf(" %d + %d is %d\n",a,b,c);
+  return 0;
+}
